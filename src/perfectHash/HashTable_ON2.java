c43d492c55bd09eb64ca02022c9a66fc1d0516dd
Magho <mmaghraby134@gmail.com>
2018-04-16 15:55:49 +0200

    add hastable class

---
+++ src/perfectHash/HashTable_ON2.java
@@ -0,0 +1,14 @@
+package perfectHash;
+
+public class HashTable_ON2 implements IHashTable {
+    
+    @Override
+    public void hash() {
+
+    }
+
+    @Override
+    public Comparable get(Comparable key) {
+        return null;
+    }
+}
